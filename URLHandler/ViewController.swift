e6967e54e484c1b47f7321de26dbbc21521a4e83
陈元兵 <cyb@runbey.com>
2020-03-12 21:17:05 +0800

    Initial Commit

---
+++ URLHandler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLHandler
+//
+//  Created by 陈元兵 on 2020/3/12.
+//  Copyright © 2020 陈元兵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
